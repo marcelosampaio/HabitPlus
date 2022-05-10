573be80b71610084df58993a6d421f11135a6108
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-10 11:57:29 -0300

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Marcelo Sampaio on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
